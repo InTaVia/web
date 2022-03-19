c2ef1559a21d7de274801bddee926be17ff43103
Stefan Probst <stefanprobst@posteo.de>
2022-03-19 18:38:14 +0100

    feat: add material ui theme provider and css reset

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,16 +1,31 @@
 import '@/styles/index.css';
 
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Fragment } from 'react';
 import { Provider } from 'react-redux';
 
 import { store } from '@/features/common/store';
+import { theme } from '@/styles/theme';
 
 export default function App(props: AppProps): JSX.Element {
   const { Component, pageProps } = props;
 
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <Fragment>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta name="color-scheme" content="light dark" />
+        <meta name="theme-color" content={theme.palette.primary.main} />
+      </Head>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Provider>
+    </Fragment>
   );
 }
