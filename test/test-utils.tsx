2b49565959dc3d88e242b63380faeb89c9f5a6cd
Stefan Probst <stefanprobst@posteo.de>
2022-03-24 19:04:52 +0100

    fix: add notifications container to wrapper component

--- test/test-utils.tsx
+++ test/test-utils.tsx
@@ -4,6 +4,7 @@ import type { FC } from 'react';
 import { Provider } from 'react-redux';
 
 import { store } from '@/features/common/store';
+import { Notifications } from '@/features/notifications/Notifications';
 import { createMockRouter } from '@/mocks/create-mock-router';
 
 interface WrapperProps {
@@ -24,7 +25,10 @@ export function createWrapper(args: CreateWrapperArgs): FC<WrapperProps> {
 
     return (
       <RouterContext.Provider value={mockRouter}>
-        <Provider store={store}>{children}</Provider>
+        <Provider store={store}>
+          {children}
+          <Notifications />
+        </Provider>
       </RouterContext.Provider>
     );
   }
