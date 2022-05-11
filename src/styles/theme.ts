17665ab2db9277e1da0783f5402df7e3c8d177ce
Stefan Probst <stefanprobst@posteo.de>
2022-05-11 21:21:34 +0200

    fix: avoid css custom properties as material ui theme color values

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -14,17 +14,18 @@ export const theme = createTheme({
     },
   },
   palette: {
+    /** Material UI does not support custom properties as color values. */
     primary: {
-      light: 'var(--gray)',
-      main: 'var(--green)',
-      dark: 'var(--dark-green)',
-      contrastText: 'var(--white)',
+      light: 'rgb(235 235 234)', // 'var(--gray)'
+      main: 'rgb(132 177 91)', // 'var(--green)'
+      dark: 'rgb(92 130 57)', // 'var(--dark-green)'
+      contrastText: 'rgb(255 255 255)', // 'var(--white)'
     },
     secondary: {
-      light: 'var(--blue)',
-      main: 'var(--blue)',
-      dark: 'var(--purple)',
-      contrastText: 'var(--black)',
+      light: 'rgb(35 167 234)', // 'var(--light-blue)'
+      main: 'rgb(35 167 234)', // 'var(--blue)'
+      dark: 'rgb(120 66 163)', // 'var(--purple)'
+      contrastText: 'rgb(0 0 0)', // 'var(--black)',
     },
   },
   typography: {
