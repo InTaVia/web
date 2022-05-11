41d166aa7e8148724ac4b5a234d98da1d39f3bdb
Johannes Liem <joh.liem@gmail.com>
2022-05-11 15:17:29 +0200

    removed div wrapper around map

--- src/features/geomap/MaplibreMap.tsx
+++ src/features/geomap/MaplibreMap.tsx
@@ -2,18 +2,14 @@ import 'maplibre-gl/dist/maplibre-gl.css';
 
 import maplibregl from 'maplibre-gl';
 import type { ReactNode } from 'react';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import ReactMapGL, { NavigationControl, ScaleControl } from 'react-map-gl';
-
-import styles from '@/features/geomap/maplibremap.module.css';
 
 interface MapProps {
   children: ReactNode;
 }
 
 export function MapLibre(props: MapProps): JSX.Element {
-  const parent = useRef<HTMLDivElement>(null);
-
   const [mapViewport, setMapViewport] = useState({
     longitude: 7.571606,
     latitude: 50.226913,
@@ -21,22 +17,20 @@ export function MapLibre(props: MapProps): JSX.Element {
   });
 
   return (
-    <div className={styles['map-wrapper']} ref={parent}>
-      <ReactMapGL
-        reuseMaps
-        mapLib={maplibregl}
-        initialViewState={mapViewport}
-        style={{ width: '100%', height: '100%' }}
-        mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
-        // mapStyle="https://demotiles.maplibre.org/style.json"
-        onMove={(e) => {
-          return setMapViewport(e.viewState);
-        }}
-      >
-        <NavigationControl />
-        <ScaleControl />
-        {props.children}
-      </ReactMapGL>
-    </div>
+    <ReactMapGL
+      reuseMaps
+      mapLib={maplibregl}
+      initialViewState={mapViewport}
+      style={{ width: '100%', height: '100%' }}
+      mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+      // mapStyle="https://demotiles.maplibre.org/style.json"
+      onMove={(e) => {
+        return setMapViewport(e.viewState);
+      }}
+    >
+      <NavigationControl />
+      <ScaleControl />
+      {props.children}
+    </ReactMapGL>
   );
 }
