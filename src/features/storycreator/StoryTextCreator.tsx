3ab4a4b51a775104c3663a93259ec47da71cb398
Vokabelsalat <jakob.kusnick@hotmail.de>
2022-05-11 14:32:01 +0200

    Allow download of story json file

--- src/features/storycreator/StoryTextCreator.tsx
+++ src/features/storycreator/StoryTextCreator.tsx
@@ -1,7 +1,7 @@
 import '~/node_modules/react-grid-layout/css/styles.css';
 import '~/node_modules/react-resizable/css/styles.css';
 
-import { TextareaAutosize } from '@mui/material';
+import { Button, TextareaAutosize } from '@mui/material';
 import { Responsive, WidthProvider } from 'react-grid-layout';
 
 import { useAppDispatch, useAppSelector } from '@/features/common/store';
@@ -26,9 +26,21 @@ export default function StoryTextCreator(props): JSX.Element {
 
   const storyObject = { ...story, slides: slideOutput };
 
+  const download = () => {
+    const element = document.createElement('a');
+    const textFile = new Blob([JSON.stringify(storyObject)], { type: 'text/plain' }); //pass data from localStorage API to blob
+    element.href = URL.createObjectURL(textFile);
+    element.download = `${storyObject['title']}_story_export.txt`;
+    document.body.appendChild(element);
+    element.click();
+  };
+
   return (
-    <TextareaAutosize className={styles['story-textarea']}>
-      {JSON.stringify(storyObject, null, 2)}
-    </TextareaAutosize>
+    <div>
+      <TextareaAutosize className={styles['story-textarea']}>
+        {JSON.stringify(storyObject, null, 2)}
+      </TextareaAutosize>
+      <Button onClick={download}>Download</Button>
+    </div>
   );
 }
