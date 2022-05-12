625b3b5d0f31a723adf0887cb61593b740041379
stefanprobst <stefanprobst@posteo.de>
2022-05-12 20:04:38 +0200

    chore: add types to ExcelUpload (#32)

--- src/features/excel-upload/ExcelUpload.tsx
+++ src/features/excel-upload/ExcelUpload.tsx
@@ -1,8 +1,9 @@
-import '~/node_modules/react-grid-layout/css/styles.css';
-import '~/node_modules/react-resizable/css/styles.css';
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
 
 import UploadFileOutlinedIcon from '@mui/icons-material/UploadFileOutlined';
 import { IconButton, Input } from '@mui/material';
+import type { ChangeEvent } from 'react';
 import * as XLSX from 'xlsx';
 
 import { addLocalEntity } from '@/features/common/entities.slice';
@@ -13,77 +14,79 @@ import styles from '@/features/storycreator/storycreator.module.css';
 export function ExcelUpload(): JSX.Element {
   const dispatch = useAppDispatch();
 
-  const processData = (dataString) => {
-    const dataStringLines = dataString.split(/\r\n|\n/);
-    const headers: string | null | undefined = dataStringLines[0].split(
-      /,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/,
-    );
+  function processData(data: string): void {
+    const [firstLine, ...lines] = data.split(/\r\n|\n/);
+    if (firstLine == null) return;
 
-    const list = [];
-    for (let i = 1; i < dataStringLines.length; i++) {
-      const row = dataStringLines[i].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
-      if (headers !== null && headers !== undefined) {
-        if (row.length == headers.length) {
-          const obj = {};
-          for (let j = 0; j < headers.length; j++) {
-            let d = row[j];
-            if (d.length > 0) {
-              if (d[0] == '"') d = d.substring(1, d.length - 1);
-              if (d[d.length - 1] == '"') d = d.substring(d.length - 2, 1);
+    const headers = firstLine.split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+
+    const list: Array<any> = [];
+
+    lines.forEach((line) => {
+      const row = line.split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+
+      if (row.length === headers.length) {
+        const obj: Record<string, string> = {};
+
+        headers.forEach((header, index) => {
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          let d = row[index]!;
+
+          if (d.length > 0) {
+            if (d[0] === '"') {
+              d = d.substring(1, d.length - 1);
             }
-            if (headers[j]) {
-              obj[headers[j]] = d;
+            if (d[d.length - 1] === '"') {
+              d = d.substring(d.length - 2, 1);
             }
           }
 
-          // remove the blank rows
-          if (
-            Object.values(obj).filter((x) => {
-              return x;
-            }).length > 0
-          ) {
-            list.push(obj);
-          }
+          obj[header] = d;
+        });
+
+        /** Remove the blank rows. */
+        if (Object.values(obj).filter(Boolean).length > 0) {
+          list.push(obj);
         }
       }
-    }
+    });
 
-    // prepare columns list from headers
-    /* const columns = headers.map((c) => {
-      return {
-        name: c,
-        selector: c,
-      };
-    }); */
+    /** Prepare columns list from headers */
+    // const columns = headers.map((c) => {
+    //   return {
+    //     name: c,
+    //     selector: c,
+    //   };
+    // });
 
     convertData(list);
-  };
+  }
 
-  function convertData(data) {
+  function convertData(data: Array<any>) {
     const events = [];
+
     for (const raw of data) {
-      const newPlace = {
+      // FIXME: Missing id and description for Place
+      const newPlace: any = {
         name: raw['Place Name'],
         lat: raw['Lat'] === '' ? null : raw['Lat'],
         lng: raw['Lon'] === '' ? null : raw['Lon'],
         kind: 'place',
       };
 
-      const newDate = raw['Event Start'];
-
-      const newDescription = raw['Event Description'];
-
-      const newEvent = {
+      // FIXME: `Relation` has `type` but not `kind`, `name`, `description`
+      const newEvent: any = {
         place: newPlace,
-        date: newDate,
-        description: newDescription,
+        date: raw['Event Start'],
+        description: raw['Event Description'],
         name: raw['Event ID'].split(':')[1],
         kind: 'event',
       };
+
       events.push(newEvent);
     }
 
-    const person = {
+    const person: Person = {
       name: 'Pier Paolo Vergerio',
       kind: 'person',
       gender: 'Male',
@@ -92,26 +95,36 @@ export function ExcelUpload(): JSX.Element {
       occupation: [],
       categories: [],
       description: '',
-    } as Person;
+    };
 
     dispatch(addLocalEntity(person));
   }
 
-  function handleFileUpload(e) {
-    const file = e.target.files[0];
+  function handleFileUpload(event: ChangeEvent<HTMLInputElement>) {
+    const fileList = event.target.files;
+    if (fileList == null) return;
+    const [file] = fileList;
+    if (file == null) return;
     const reader = new FileReader();
+
     reader.onload = (evt) => {
-      /* Parse data */
-      const bstr = evt.target.result;
-      const wb = XLSX.read(bstr, { type: 'binary' });
-      /* Get first worksheet */
-      const wsname = wb.SheetNames[0];
-      const ws = wb.Sheets[wsname];
-      /* Convert array of arrays */
+      /** Parse data. */
+      const bstr = evt.target?.result;
+      const workbook = XLSX.read(bstr, { type: 'binary' });
+
+      /** Get first worksheet. */
+      const [worksheetName] = workbook.SheetNames;
+      if (worksheetName == null) return;
+
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const ws = workbook.Sheets[worksheetName]!;
+      /** Convert array of arrays. */
+      // @ts-expect-error FIXME: `header` is either missing from types, or not a valid option
       const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
 
       processData(data);
     };
+
     reader.readAsBinaryString(file);
   }
 
@@ -119,6 +132,7 @@ export function ExcelUpload(): JSX.Element {
     <div className={styles['button-row-button']}>
       <label htmlFor="icon-button-file">
         <Input
+          // @ts-expect-error FIXME: check why this is missing in material ui types
           accept=".csv,.xlsx,.xls"
           id="icon-button-file"
           type="file"
