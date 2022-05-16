109c0617afad31fdc952b81acac3ad0da12e5449
Stefan Probst <stefanprobst@posteo.de>
2022-05-16 14:59:28 +0200

    refactor: integrate getPersonsByParam endpoint in getPersons

--- src/mocks/intavia-api.mocks.ts
+++ src/mocks/intavia-api.mocks.ts
@@ -10,8 +10,20 @@ export const handlers = [
   rest.get<never, never, { page: number; pages: number; entities: Array<Person> }>(
     String(createIntaviaApiUrl({ pathname: '/api/persons' })),
     (request, response, context) => {
-      const q = request.url.searchParams.get('q')?.trim() ?? undefined;
-      const persons = db.person.findMany(q);
+      const q = getSearchParam(request.url, 'q');
+      const dateOfBirthStart = getSearchParam(request.url, 'dateOfBirthStart');
+      const dateOfBirthEnd = getSearchParam(request.url, 'dateOfBirthEnd');
+      const dateOfDeathStart = getSearchParam(request.url, 'dateOfDeathStart');
+      const dateOfDeathEnd = getSearchParam(request.url, 'dateOfBirthEnd');
+      const start =
+        dateOfBirthStart != null && dateOfBirthEnd != null
+          ? ([Number(dateOfBirthStart), Number(dateOfBirthEnd)] as [number, number])
+          : undefined;
+      const end =
+        dateOfDeathStart != null && dateOfDeathEnd != null
+          ? ([Number(dateOfDeathStart), Number(dateOfDeathEnd)] as [number, number])
+          : undefined;
+      const persons = db.person.findMany(q, start, end);
 
       const limit = 10;
       const pages = Math.ceil(persons.length / limit);
@@ -31,48 +43,6 @@ export const handlers = [
         context.status(200),
         context.delay(),
         context.json({ page, pages, q, entities }),
-      );
-    },
-  ),
-  rest.get<never, never, { page: number; pages: number; entities: Array<Person> }>(
-    String(createIntaviaApiUrl({ pathname: '/api/persons/byParam' })),
-    (request, response, context) => {
-      const name = request.url.searchParams.get('name')?.trim() ?? undefined;
-      const dateOfBirthStart =
-        request.url.searchParams.get('dateOfBirthStart')?.trim() ?? undefined;
-      const dateOfBirthEnd = request.url.searchParams.get('dateOfBirthEnd')?.trim() ?? undefined;
-      const dateOfDeathStart =
-        request.url.searchParams.get('dateOfDeathStart')?.trim() ?? undefined;
-      const dateOfDeathEnd = request.url.searchParams.get('dateOfDeathEnd')?.trim() ?? undefined;
-      const dateOfBirthRange =
-        dateOfBirthStart !== undefined && dateOfBirthEnd !== undefined
-          ? [Number(dateOfBirthStart), Number(dateOfBirthEnd)]
-          : undefined;
-      const dateOfDeathRange =
-        dateOfDeathStart !== undefined && dateOfDeathEnd !== undefined
-          ? [Number(dateOfDeathStart), Number(dateOfDeathEnd)]
-          : undefined;
-
-      const persons = db.person.findByParams(dateOfBirthRange, dateOfDeathRange, name);
-
-      const limit = 10;
-      const pages = Math.ceil(persons.length / limit);
-      const page = clamp(Number(request.url.searchParams.get('page') ?? 1), 1, pages);
-      const offset = (page - 1) * limit;
-
-      const entities = persons.slice(offset, offset + limit).map((person) => {
-        return {
-          ...person,
-          history: person.history?.map((relation) => {
-            return { ...relation, place: db.place.findById(relation.placeId!) };
-          }),
-        };
-      });
-
-      return response(
-        context.status(200),
-        context.delay(),
-        context.json({ page, pages, entities }),
       );
     },
   ),
@@ -123,7 +93,7 @@ export const handlers = [
   rest.get<never, never, { page: number; pages: number; entities: Array<Place> }>(
     String(createIntaviaApiUrl({ pathname: '/api/places' })),
     (request, response, context) => {
-      const q = request.url.searchParams.get('q')?.trim() ?? undefined;
+      const q = getSearchParam(request.url, 'q');
       const places = db.place.findMany(q);
 
       const limit = 10;
@@ -155,3 +125,8 @@ export const handlers = [
     },
   ),
 ];
+
+function getSearchParam(url: URL, key: string): string | undefined {
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  return url.searchParams.get(key)?.trim() || undefined;
+}
