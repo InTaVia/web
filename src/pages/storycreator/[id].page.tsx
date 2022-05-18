6fe7f31e98ece67dd5f3039d03d11b9bba57c035
Vokabelsalat <jakob.kusnick@hotmail.de>
2022-05-18 13:38:14 +0200

    Fix useParams instead of searchParams for stories

--- src/pages/storycreator/[id].page.tsx
+++ src/pages/storycreator/[id].page.tsx
@@ -5,24 +5,23 @@ import { useEffect } from 'react';
 import { useAppSelector } from '@/features/common/store';
 import { StoryCreator } from '@/features/storycreator/StoryCreator';
 import { selectStories } from '@/features/storycreator/storycreator.slice';
-import { useSearchParams } from '@/lib/use-search-params';
+import { useParams } from '@/lib/use-params';
 
 export default function StoryPage(): JSX.Element | null {
   const router = useRouter();
-  const searchParams = useSearchParams();
+  const params = useParams();
+  const id = params?.get('id');
+
   const stories = useAppSelector(selectStories);
 
-  const id = searchParams?.get('id');
   const story = id != null ? stories[id] : null;
 
   useEffect(() => {
     /** Router is not ready yet. */
-    if (searchParams == null) return;
-
     if (story == null) {
       void router.replace({ pathname: '/storycreator' });
     }
-  }, [router, searchParams, story]);
+  }, [router, story]);
 
   if (story == null) {
     return null;
