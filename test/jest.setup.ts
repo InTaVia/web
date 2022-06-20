6863295696f2227e81e5b74bbf39042452aa6f9c
Stefan Probst <stefanprobst@posteo.de>
2022-06-20 14:40:17 +0200

    test: mock global.URL.createObjectURL which is not available in jsdom

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -1,3 +1,6 @@
 import '@testing-library/jest-dom';
 /** Apply built-in polyfills like `fetch`. */
 import 'next';
+
+/** @see https://github.com/jsdom/jsdom/issues/1721 */
+global.URL.createObjectURL = jest.fn();
