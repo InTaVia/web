21298c5c77ded7882e6516f1c69886657568347e
Max Franke <Max.Franke@vis.uni-stuttgart.de>
2022-07-06 15:57:35 +0200

    [wip] show entity type (mocked) by color and symbol

--- src/features/entities/collection-panel-entry.tsx
+++ src/features/entities/collection-panel-entry.tsx
@@ -1,57 +1,75 @@
 import { Disclosure } from '@headlessui/react';
+import { LocationMarkerIcon, UserCircleIcon } from '@heroicons/react/solid';
 import { Fragment } from 'react';
 
 import type { Entity } from '@/features/common/entity.model';
+import { eventTypes } from '@/features/common/event-types';
+import { formatDate } from '@/lib/format-date';
 
 export interface CollectionPanelEntryProps {
   entity: Entity;
 }
 
 export default function CollectionPanelEntry(props: CollectionPanelEntryProps): JSX.Element {
+  //const isPerson = props.entity.kind === 'person';
+  const isPerson = parseInt(props.entity.id) % 2 === 0;
+  const [fgColor, bgColor, symbol] = isPerson
+    ? ['text-pink-900', 'bg-pink-100', <UserCircleIcon />]
+    : ['text-blue-900', 'bg-blue-100', <LocationMarkerIcon />];
   return (
-    <div className="my-2 rounded border-2 border-red-500 p-2">
+    <div className={`my-2 mx-1 rounded border-2 border-current p-2 ${fgColor} ${bgColor}`}>
       <Disclosure>
         {({ open }) => {
           return (
             <Fragment>
               <Disclosure.Button className="display-block py-2">
-                <div className="grid grid-cols-[3.6rem_1fr_minmax(50px,max-content)] grid-rows-3 gap-1">
-                  <svg className="col-start-1 row-span-3 row-start-1" viewBox="0 0 1 1">
-                    <path
-                      fill="rebeccapurple"
-                      d="M0 1 L 0.35 0.65 V 0.4 H 0.3 V 0.1 H 0.7 V 0.4 H 0.65 V 0.65 L 1 1 Z"
-                    />
-                  </svg>
+                <div className="mb-3 grid grid-cols-[3.6rem_1fr_minmax(50px,max-content)] grid-rows-3 gap-1">
+                  <div className={`col-start-1 row-span-3 row-start-1 ${fgColor}`}>{symbol}</div>
                   <h2 className="col-start-2 row-span-3 row-start-1 my-1 place-self-center text-lg font-semibold">
                     {props.entity.name}
                   </h2>
-                  <span className="font-verythin col-start-3 row-start-1 max-w-[20ch] overflow-hidden text-ellipsis whitespace-nowrap text-[0.5rem]">
+                  <span className="font-verythin col-start-3 row-start-1 max-w-[20ch] justify-self-end overflow-hidden text-ellipsis whitespace-nowrap text-[0.65rem]">
                     {props.entity.kind === 'person' ? props.entity.categories.join(', ') : ''}
                   </span>
-                  <span className="font-verythin col-start-3 row-start-2 max-w-[20ch] overflow-hidden text-ellipsis whitespace-nowrap text-[0.5rem]">
+                  <span className="font-verythin  col-start-3 row-start-2 max-w-[20ch] justify-self-end overflow-hidden text-ellipsis whitespace-nowrap text-[0.65rem]">
                     {props.entity.kind === 'person'
                       ? props.entity.gender
                       : props.entity.kind === 'place'
                       ? `${props.entity.lat} ${props.entity.lng}`
                       : ``}
                   </span>
-                  <span className="font-verythin col-start-3 row-start-3 max-w-[20ch] overflow-hidden text-ellipsis whitespace-nowrap text-[0.5rem]">
+                  <span className="font-verythin col-start-3 row-start-3 max-w-[20ch] justify-self-end overflow-hidden text-ellipsis whitespace-nowrap text-[0.65rem]">
                     {props.entity.kind === 'person' ? props.entity.occupation.join(', ') : ''}
                   </span>
                 </div>
 
-                <div className={open ? '' : 'text-gray-500 line-clamp-3'}>
+                <div className={`${open ? '' : `contrast-[.2] line-clamp-3`} text-justify`}>
                   <p>{props.entity.description}</p>
                 </div>
               </Disclosure.Button>
-              <Disclosure.Panel className="text-gray-500">
-                <h3 className="text-md font-semibold">Events</h3>
+              <Disclosure.Panel>
+                {props.entity.kind === 'person' ? (
+                  <Fragment>
+                    <h3 className="text-lg font-semibold">Events</h3>
 
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptate, ducimus
-                  minus, architecto nemo atque nisi odit dolorum adipisci deserunt fugiat quae quas
-                  rem asperiores neque nostrum est. A, delectus!
-                </p>
+                    <div className="grid grid-cols-[max-content_1fr] justify-items-start gap-1">
+                      {props.entity.history?.map((event, index) => {
+                        // TODO: events should have label and description
+                        return (
+                          <Fragment key={index}>
+                            <span className="font-semibold">{eventTypes[event.type].label}</span>{' '}
+                            <span>
+                              {event.date != null ? (
+                                <span>{formatDate(new Date(event.date))}</span>
+                              ) : null}{' '}
+                              {event.place != null ? <span>in {event.place.name}</span> : null}
+                            </span>
+                          </Fragment>
+                        );
+                      })}
+                    </div>
+                  </Fragment>
+                ) : null}
               </Disclosure.Panel>
             </Fragment>
           );
