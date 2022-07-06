b0aa28f2043cf2959d2ddd7d2dd5d132e313a50d
Samuel Beck <samuel.beck@vis.uni-stuttgart.de>
2022-07-06 13:03:42 +0200

    feat: add app bar (#73)
    * Remove drawer from page layout
    
    * Fix exlint errors
    
    * Add empty pages for main components
    
    * Temporarily comment out internationalization for title of new pages
    
    * Fix eslint errors
    
    * Highlight current page in app bar
    
    * Add link to home page to intavia logo in app bar
    
    * Commit lock file because of new dependencies
    
    * Address pr review comments
    
    * chore: don't add aria-current for non-active pages
    
    Co-authored-by: Stefan Probst <stefanprobst@posteo.de>

---
+++ src/pages/visual-analytics-studio.page.tsx
@@ -0,0 +1,25 @@
+import { PageMetadata } from '@stefanprobst/next-page-metadata';
+import { Fragment } from 'react';
+
+// import { useI18n } from '@/app/i18n/use-i18n';
+import { withDictionaries } from '@/app/i18n/with-dictionaries';
+import { usePageTitleTemplate } from '@/app/metadata/use-page-title-template';
+
+export const getStaticProps = withDictionaries(['common']);
+
+export default function VisualAnalyticsStudioPage(): JSX.Element {
+  // const { t } = useI18n<'common'>();
+  const titleTemplate = usePageTitleTemplate();
+
+  // t(['common', 'visual-analytics-studio', 'metadata', 'title'])
+  const metadata = { title: 'visual-analytics-studio' };
+
+  return (
+    <Fragment>
+      <PageMetadata title={metadata.title} titleTemplate={titleTemplate} />
+      <main>
+        <h1>Visual Analytics Studio</h1>
+      </main>
+    </Fragment>
+  );
+}
