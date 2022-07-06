2b99f0f06e406a3173aa421a9a8dfe67a514533e
Johannes Liem <joh.liem@gmail.com>
2022-07-06 13:11:22 +0200

    feat: add shared application layout (#75)
    * feat: application layout vas
    
    * Fix typescript errors (partially)
    
    Co-authored-by: Stefan Probst <stefanprobst@posteo.de>

--- src/pages/visual-analytics-studio.page.tsx
+++ src/pages/visual-analytics-studio.page.tsx
@@ -1,25 +1,120 @@
-import { PageMetadata } from '@stefanprobst/next-page-metadata';
-import { Fragment } from 'react';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
+import { Allotment } from 'allotment';
+import { useState } from 'react';
+import ReactResizeDetector from 'react-resize-detector';
 
-// import { useI18n } from '@/app/i18n/use-i18n';
-import { withDictionaries } from '@/app/i18n/with-dictionaries';
-import { usePageTitleTemplate } from '@/app/metadata/use-page-title-template';
+import { CollectionEntitiesList } from '@/features/data-view-panel/data-view-panel';
+import AnalysePageToolbar from '@/features/ui/analyse-page-toolbar/toolbar';
+import SidePane from '@/features/ui/side-pane';
 
-export const getStaticProps = withDictionaries(['common']);
-
-export default function VisualAnalyticsStudioPage(): JSX.Element {
-  // const { t } = useI18n<'common'>();
-  const titleTemplate = usePageTitleTemplate();
-
-  // t(['common', 'visual-analytics-studio', 'metadata', 'title'])
-  const metadata = { title: 'visual-analytics-studio' };
+export default function AnalysePage(): JSX.Element {
+  const [visibleLeftPane, setVisibleLeftPane] = useState(true);
+  const [visibleRightPane, setVisibleRightPane] = useState(true);
 
   return (
-    <Fragment>
-      <PageMetadata title={metadata.title} titleTemplate={titleTemplate} />
-      <main>
-        <h1>Visual Analytics Studio</h1>
-      </main>
-    </Fragment>
+    <Allotment
+      vertical={false}
+      onVisibleChange={(index, value) => {
+        if (index === 0) {
+          setVisibleLeftPane(value);
+        } else if (index === 2) {
+          setVisibleRightPane(value);
+        }
+      }}
+    >
+      {visibleLeftPane === true ? (
+        <Allotment.Pane
+          key={`allotmentLeft${visibleLeftPane}`}
+          preferredSize="15%"
+          minSize={400}
+          maxSize={400}
+          visible={visibleLeftPane}
+          snap
+        >
+          <SidePane setVisible={setVisibleLeftPane}>
+            <div disclosureTitle="Data">
+              <CollectionEntitiesList />
+            </div>
+            <div disclosureTitle="Search History">Test</div>
+          </SidePane>
+        </Allotment.Pane>
+      ) : (
+        <Allotment.Pane minSize={20} maxSize={20}>
+          <button
+            key="minimizeButton"
+            onClick={() => {
+              setVisibleLeftPane(true);
+            }}
+          >
+            <ChevronRightIcon className="h-5 w-5" />
+          </button>
+        </Allotment.Pane>
+      )}
+
+      <Allotment.Pane key="allotmentCenter" preferredSize="70%" minSize={400}>
+        <div className="grid grid-rows-[auto_1fr] bg-blue-500 min-h-full">
+          <AnalysePageToolbar
+            onLayoutSelected={(key) => {
+              return console.log('layout selected:', key);
+            }}
+          ></AnalysePageToolbar>
+          <div>Content</div>
+          <div>Tabbar</div>
+        </div>
+      </Allotment.Pane>
+
+      {visibleRightPane === true ? (
+        <Allotment.Pane
+          key={`allotmentRight${visibleRightPane}`}
+          preferredSize="15%"
+          minSize={20}
+          visible={visibleRightPane}
+        >
+          {/* <SidePane orientation="right" setVisible={setVisibleRightPane}>
+            <div disclosureTitle="Placeholder">Placeholder</div>
+            <div disclosureTitle="Placeholder">Placeholder</div>
+          </SidePane> */}
+          <ReactResizeDetector handleWidth handleHeight>
+            {({ width }) => {
+              if (width > 200) {
+                return (
+                  <SidePane orientation="right" setVisible={setVisibleRightPane}>
+                    <div disclosureTitle="Placeholder">Placeholder</div>
+                    <div disclosureTitle="Placeholder">Placeholder</div>
+                  </SidePane>
+                );
+              } else {
+                return (
+                  <div>
+                    <button
+                      key="minimizeButton"
+                      onClick={() => {
+                        setVisibleRightPane(true);
+                      }}
+                    >
+                      <ChevronLeftIcon className="h-5 w-5" />
+                    </button>
+                  </div>
+                );
+              }
+            }}
+          </ReactResizeDetector>
+        </Allotment.Pane>
+      ) : (
+        <Allotment.Pane minSize={20} maxSize={20}>
+          <button
+            key="minimizeButton"
+            onClick={() => {
+              setVisibleRightPane(true);
+            }}
+          >
+            <ChevronLeftIcon className="h-5 w-5" />
+          </button>
+        </Allotment.Pane>
+      )}
+      {/* <Allotment>
+          <Allotment.Pane preferredSize="20%"></Allotment.Pane>
+        </Allotment> */}
+    </Allotment>
   );
 }
