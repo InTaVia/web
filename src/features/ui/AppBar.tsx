57aad1ae4ac7edadb9bad30b8c51f909fb686600
Samuel Beck <samuel.beck@vis.uni-stuttgart.de>
2022-07-07 11:28:32 +0200

    Internationalize app bar

--- src/features/ui/AppBar.tsx
+++ src/features/ui/AppBar.tsx
@@ -2,8 +2,12 @@ import { InformationCircleIcon, UploadIcon } from '@heroicons/react/outline';
 import { clsx } from 'clsx';
 import Image from 'next/image';
 
+import { useI18n } from '@/app/i18n/use-i18n';
+import { withDictionaries } from '@/app/i18n/with-dictionaries';
 import { usePathname } from '@/app/route/use-pathname';
 import IntaviaLogo from '~/public/assets/images/logo.svg';
+
+export const getStaticProps = withDictionaries(['common']);
 
 interface Link {
   id: string;
@@ -13,25 +17,26 @@ interface Link {
 }
 
 export function AppBar(): JSX.Element {
+  const { t } = useI18n<'common'>();
   const currentPath = usePathname();
 
   const linksLeft: Array<Link> = [
     {
       id: 'data-curation-lab',
       href: { pathname: '/search' },
-      label: 'Data Curation Lab',
+      label: t(['common', 'data-curation-lab']),
       current: false,
     },
     {
       id: 'visual-analytics-studio',
       href: { pathname: '/visual-analytics-studio' },
-      label: 'Visual Analytics Studio',
+      label: t(['common', 'visual-analytics-studio']),
       current: false,
     },
     {
       id: 'storytelling-creator',
       href: { pathname: '/storycreator' },
-      label: 'Storytelling Creator',
+      label: t(['common', 'storytelling-creator']),
       current: false,
     },
   ];
@@ -53,15 +58,15 @@ export function AppBar(): JSX.Element {
   }
 
   return (
-    <div className="w-full h-16 bg-white">
+    <div className="h-16 w-full bg-white">
       <div className="flex flex-row flex-nowrap justify-between">
         <div className="flex flex-row items-center gap-2">
-          <div className="h-14 w-40 relative">
+          <div className="relative h-14 w-40">
             <a href="/">
               <Image src={IntaviaLogo} layout="fill" objectFit="contain" />
             </a>
           </div>
-          <div className="flex flex-row h-16 gap-3 items-center">
+          <div className="flex h-16 flex-row items-center gap-3">
             {linksLeft.map((item) => {
               return (
                 <a
@@ -69,7 +74,7 @@ export function AppBar(): JSX.Element {
                   href={item.href.pathname}
                   className={clsx(
                     item.current ? 'text-intavia-brand' : 'text-black',
-                    'hover:text-intavia-brand px-3 text-base',
+                    'px-3 text-base hover:text-intavia-brand',
                   )}
                   aria-current={item.current ? 'page' : undefined}
                 >
@@ -79,10 +84,10 @@ export function AppBar(): JSX.Element {
             })}
           </div>
         </div>
-        <div className="flex flex-row h-16 gap-6 items-center pr-6">
-          <button className="bg-intavia-brand px-4 py-1 text-white rounded-lg flex items-center gap-2 text-base hover:text-intavia-brand hover:bg-white hover:border hover:border-intavia-brand">
+        <div className="flex h-16 flex-row items-center gap-6 pr-6">
+          <button className="flex items-center gap-2 rounded-lg bg-intavia-brand px-4 py-1 text-base text-white hover:border hover:border-intavia-brand hover:bg-white hover:text-intavia-brand">
             <UploadIcon className="h-5 w-5" strokeWidth="1.75" />
-            Data Import
+            {t(['common', 'data-import'])}
           </button>
 
           {linksRight.map((item) => {
