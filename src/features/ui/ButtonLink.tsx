84673fe82494b47fea385ff9b0cbbdcaaeb47c5b
Max Franke <Max.Franke@vis.uni-stuttgart.de>
2022-07-07 14:05:43 +0200

    add link styled like button, refactor button classes to be available in link as well

---
+++ src/features/ui/ButtonLink.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import type { AnchorHTMLAttributes } from 'react';
+
+import type { FullButtonProperties } from '@/features/ui/Button';
+import { getButtonClasses } from '@/features/ui/Button';
+
+export type ButtonLinkProperties = Partial<FullButtonProperties> &
+  Pick<FullButtonProperties, 'children'> & { href: string };
+
+export default function ButtonLink(props: ButtonLinkProperties): JSX.Element {
+  const { href, ...otherProps } = props;
+  const { className, children, disabled: _disabled, extraProps } = getButtonClasses(otherProps);
+
+  return (
+    <Link href={href} passHref>
+      <a className={className} {...(extraProps as AnchorHTMLAttributes<HTMLAnchorElement>)}>
+        {children}
+      </a>
+    </Link>
+  );
+}
