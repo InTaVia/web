005e78bd6c5764ec809f123312e3986913d9af04
stefanprobst <stefanprobst@posteo.de>
2022-08-16 11:27:55 +0200

    fix: remove stray component render in test

--- test/pages/index.page.test.tsx
+++ test/pages/index.page.test.tsx
@@ -4,7 +4,6 @@ import HomePage from '@/pages/index.page';
 import { createWrapper } from '~/test/test-utils';
 
 describe('HomePage', () => {
-  <HomePage />;
   it('should display page title', () => {
     const router = { pathname: '/' };
     render(<HomePage />, { wrapper: createWrapper({ router }) });
