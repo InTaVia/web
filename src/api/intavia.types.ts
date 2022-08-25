092b2d2ea83a89d104a33c31751e914eca26cea8
Johannes Liem <joh.liem@gmail.com>
2022-08-25 06:50:48 +0200

    xls import and data tranformation

---
+++ src/api/intavia.types.ts
@@ -0,0 +1,41 @@
+export interface Bin<T = number> {
+  label: string;
+  count: number;
+  values: [T, T];
+  order?: number;
+}
+
+export interface Node<T extends { id: string }> {
+  id: T['id'];
+  count: number;
+  children: Array<Node<T>>;
+}
+
+export interface RootNode<T extends { id: string }> extends Node<T> {
+  id: 'root';
+  count: 0;
+  children: Array<Node<T>>;
+}
+
+export interface InternationalizedLabel {
+  default: string;
+  de?: string;
+  du?: string;
+  en?: string;
+  fi?: string;
+  si?: string;
+}
+
+export type PaginatedRequest<T> = T & {
+  /** @default 1 */
+  page?: number;
+  /** @default 50 */
+  limit?: number;
+};
+
+export interface PaginatedResponse<T> {
+  count: number;
+  page: number;
+  pages: number;
+  results: Array<T>;
+}
