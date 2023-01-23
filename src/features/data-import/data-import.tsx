11c1213022827d7607a91452efd3a2df39acde26
Johannes Liem <joh.liem@gmail.com>
2023-01-23 22:16:43 +0100

    update data-import layout

--- src/features/data-import/data-import.tsx
+++ src/features/data-import/data-import.tsx
@@ -7,18 +7,15 @@ export function DataImport(): JSX.Element {
   const [importedData, setImportedData] = useState<ImportData | null>(null);
 
   return (
-    <div className="grid h-full grid-rows-[auto_1fr_auto]">
+    <div className="grid h-full grid-cols-1 grid-rows-[auto_1fr_auto]">
       <div className="bg-slate-400 p-2">
         <h1 className="text-2xl">Data Import</h1>
         <DataImportForm setImportedData={setImportedData} />
       </div>
-
-      <div className="grid h-full grid-rows-[auto_1fr]">
-        <div className="bg-slate-300 p-2">Summary</div>
-        <div className="overflow-hidden overflow-y-scroll">
-          {importedData && JSON.stringify(importedData, null, 2)}
-        </div>
+      <div className="overflow-hidden overflow-y-scroll">
+        <div className="bg-indigo-300">{importedData && JSON.stringify(importedData, null, 2)}</div>
       </div>
+
       <div className="bg-slate-300 p-4">Option to load into collection; Import Button</div>
     </div>
   );
