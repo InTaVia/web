2d66cacf29b879adee197c4651cee1aeedc2c038
Stefan Probst <stefanprobst@posteo.de>
2023-03-06 12:07:48 +0100

    fix: use http error message in toast

--- src/app/store/error.middleware.ts
+++ src/app/store/error.middleware.ts
@@ -1,12 +1,14 @@
 import { toast } from '@intavia/ui';
 import type { Action, Middleware } from '@reduxjs/toolkit';
 import { isRejectedWithValue } from '@reduxjs/toolkit';
+import { HttpError } from '@stefanprobst/request';
 
 const middleware: Middleware = function errorMiddleware() {
   return (next) => {
     return (action: Action) => {
       if (isRejectedWithValue(action)) {
-        const description = action.error.message;
+        const description =
+          action.payload instanceof HttpError ? action.payload.message : action.error.message;
 
         toast({
           title: 'Error',
