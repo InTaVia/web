eb1eb5e824dbef1bb7e0c50998614b604553dc62
Stefan Probst <stefanprobst@posteo.de>
2023-03-06 14:05:11 +0100

    refactor: use select from intavia ui

--- src/features/common/component-properties-dialog.tsx
+++ src/features/common/component-properties-dialog.tsx
@@ -1,10 +1,19 @@
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 
-import { Dialog, Disclosure, Listbox, Transition } from '@headlessui/react';
-import { CheckIcon, ChevronUpIcon, SelectorIcon, TrashIcon } from '@heroicons/react/solid';
+import { Dialog, Disclosure, Transition } from '@headlessui/react';
+import { ChevronUpIcon, TrashIcon } from '@heroicons/react/solid';
 import type { Event } from '@intavia/api-client';
-import { Input, Switch, Textarea } from '@intavia/ui';
+import {
+  Input,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  Switch,
+  Textarea,
+} from '@intavia/ui';
 import { Fragment, useState } from 'react';
 
 import { useAppSelector } from '@/app/store';
@@ -170,10 +179,10 @@ export function ComponentPropertiesDialog(props: ComponentPropertiesDialogProps)
                         case 'select':
                           return [
                             <div key={`${property.id}Label`}>{property.label}</div>,
-                            <Listbox
-                              key={`${property.id}Listbox`}
+                            <Select
+                              key={`${property.id}Select`}
                               value={property.value}
-                              onChange={(val: any) => {
+                              onValueChange={(val: any) => {
                                 const newProps = {
                                   ...tmpProperties,
                                 };
@@ -182,57 +191,22 @@ export function ComponentPropertiesDialog(props: ComponentPropertiesDialogProps)
                                 setTmpProperties(newProps);
                               }}
                             >
-                              <div className="relative mt-1">
-                                <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                              <SelectTrigger>
+                                <SelectValue>
                                   <span className="block truncate">{property.value.name}</span>
-                                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                                    <SelectorIcon
-                                      className="h-5 w-5 text-gray-400"
-                                      aria-hidden="true"
-                                    />
-                                  </span>
-                                </Listbox.Button>
-                                <Transition
-                                  as={Fragment}
-                                  leave="transition ease-in duration-100"
-                                  leaveFrom="opacity-100"
-                                  leaveTo="opacity-0"
-                                >
-                                  <Listbox.Options className="absolute z-50 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                                    {property.options?.map((option, optionIdx) => {
-                                      return (
-                                        <Listbox.Option
-                                          key={`option${optionIdx}`}
-                                          className={({ active }) => {
-                                            return `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                                              active
-                                                ? 'bg-amber-100 text-amber-900'
-                                                : 'text-gray-900'
-                                            }`;
-                                          }}
-                                          value={option}
-                                        >
-                                          <span
-                                            className={`block truncate ${
-                                              property.value !== null
-                                                ? 'font-medium'
-                                                : 'font-normal'
-                                            }`}
-                                          >
-                                            {option.name}
-                                          </span>
-                                          {property.value.value === option.value ? (
-                                            <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                                              <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                            </span>
-                                          ) : null}
-                                        </Listbox.Option>
-                                      );
-                                    })}
-                                  </Listbox.Options>
-                                </Transition>
-                              </div>
-                            </Listbox>,
+                                </SelectValue>
+                              </SelectTrigger>
+
+                              <SelectContent>
+                                {property.options?.map((option, optionIdx) => {
+                                  return (
+                                    <SelectItem key={`option${optionIdx}`} value={option}>
+                                      {option.name}
+                                    </SelectItem>
+                                  );
+                                })}
+                              </SelectContent>
+                            </Select>,
                           ];
 
                         case 'entitiesAndEvents':
