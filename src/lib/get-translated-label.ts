e287663c154c80bca571f697fe547d765ed92174
Stefan Probst <stefanprobst@posteo.de>
2023-03-08 12:16:24 +0100

    fix: handle missing label field

--- src/lib/get-translated-label.ts
+++ src/lib/get-translated-label.ts
@@ -4,8 +4,11 @@ import type { Locale } from '~/config/i18n.config';
 import { defaultLocale } from '~/config/i18n.config';
 
 export function getTranslatedLabel(
-  labels: InternationalizedLabel,
+  labels: InternationalizedLabel | undefined,
   locale: Locale = defaultLocale,
 ): string {
+  // FIXME: this should never happen, but it does because the backend does not always return
+  // a label field for events.
+  if (labels == null) return 'Unknown';
   return labels[locale] ?? labels.default;
 }
