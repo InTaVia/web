d95f4b4f34d4479510d86bb2117324d554b288f8
Stefan Probst <stefanprobst@posteo.de>
2023-03-08 13:18:22 +0100

    fix: allow clearing search facet

--- src/components/search/search-facets.tsx
+++ src/components/search/search-facets.tsx
@@ -1,3 +1,4 @@
+import { XIcon } from '@heroicons/react/solid';
 import type { EntityKind, InternationalizedLabel, SearchEntities } from '@intavia/api-client';
 import {
   Button,
@@ -22,15 +23,21 @@ import type { ChangeEvent } from 'react';
 import { useId, useMemo, useState } from 'react';
 import { useField } from 'react-final-form';
 
-import { useSearchEntitiesQuery, useSearchRelationRolesQuery } from '@/api/intavia.service';
+import {
+  useGetEntityByIdQuery,
+  useGetRelationRoleByIdQuery,
+  useSearchEntitiesQuery,
+  useSearchRelationRolesQuery,
+} from '@/api/intavia.service';
 import { useI18n } from '@/app/i18n/use-i18n';
 import { useAppSelector } from '@/app/store';
-import { selectEvents, selectVocabularyEntries } from '@/app/store/intavia.slice';
+import { selectEntities, selectVocabularyEntries } from '@/app/store/intavia.slice';
 import { Form } from '@/components/form';
 import { FormField } from '@/components/form-field';
 import { useSearchEntities } from '@/components/search/use-search-entities';
 import { useSearchEntitiesFilters } from '@/components/search/use-search-entities-filters';
 import { getTranslatedLabel } from '@/lib/get-translated-label';
+import { isNonEmptyString } from '@/lib/is-nonempty-string';
 import { useDebouncedValue } from '@/lib/use-debounced-value';
 
 export function SearchFacets(): JSX.Element {
@@ -74,31 +81,50 @@ function RelatedEntityFilter(): JSX.Element {
   const label = 'Related entity';
   const placeholder = 'Select entity';
 
+  const hasSelection = isNonEmptyString(field.input.value);
+  const storedEntities = useAppSelector(selectEntities);
+  const selectedEntityFromStore = hasSelection ? storedEntities[field.input.value] : undefined;
+  const { data: selectedEntityFromBackend, isLoading: isInitialLoading } = useGetEntityByIdQuery(
+    { id: field.input.value },
+    { skip: !hasSelection || selectedEntityFromStore != null },
+  );
+  const selected = selectedEntityFromStore ?? selectedEntityFromBackend;
+
   const [searchTerm, setSearchTerm] = useState('');
   const q = useDebouncedValue(searchTerm.trim());
   const { data, isLoading, isFetching } = useSearchEntitiesQuery({ q });
   const events = useMemo(() => {
-    return keyBy(data?.results ?? [], (e) => {
+    const events = keyBy(data?.results ?? [], (e) => {
       return e.id;
     });
-  }, [data]);
-
-  const _stored = useAppSelector(selectEvents);
+    if (selected != null) {
+      events[selected.id] = selected;
+    }
+    return events;
+  }, [data, selected]);
 
   function onInputChange(event: ChangeEvent<HTMLInputElement>) {
     setSearchTerm(event.currentTarget.value);
   }
 
   function getDisplayLabel(id: string) {
-    const event = events[id] ?? _stored[id];
+    const event = events[id] ?? storedEntities[id];
     if (event == null) return '';
     return getTranslatedLabel(event.label);
+  }
+
+  function onClear() {
+    field.input.onChange(undefined);
   }
 
   return (
     <FormField>
       <Label htmlFor={id}>{label}</Label>
-      <ComboBox disabled={isLoading} onValueChange={field.input.onChange} value={field.input.value}>
+      <ComboBox
+        disabled={isLoading || isInitialLoading}
+        onValueChange={field.input.onChange}
+        value={field.input.value}
+      >
         <ComboBoxTrigger>
           <ComboBoxInput
             displayValue={getDisplayLabel}
@@ -106,8 +132,13 @@ function RelatedEntityFilter(): JSX.Element {
             onChange={onInputChange}
             placeholder={placeholder}
           />
+          {isFetching || isInitialLoading ? (
+            <LoadingIndicator className="mr-4 text-neutral-500" size="sm" />
+          ) : null}
+          {isNonEmptyString(field.input.value) && !isFetching && !isInitialLoading ? (
+            <ClearButton className="mr-4 text-neutral-500" onClick={onClear} />
+          ) : null}
           <ComboBoxButton />
-          {isFetching ? <LoadingIndicator className="mr-4 text-neutral-500" size="sm" /> : null}
         </ComboBoxTrigger>
         <ComboBoxContent>
           {Object.values(events).map((event) => {
@@ -142,31 +173,51 @@ function RelationRoleFilter(): JSX.Element {
   const label = 'Relation role';
   const placeholder = 'Select role';
 
+  const hasSelection = isNonEmptyString(field.input.value);
+  const storedRoles = useAppSelector(selectVocabularyEntries);
+  const selectedRoleFromStore = hasSelection ? storedRoles[field.input.value] : undefined;
+  const { data: selectedRoleFrombackend, isLoading: isInitialLoading } =
+    useGetRelationRoleByIdQuery(
+      { id: field.input.value },
+      { skip: !hasSelection || selectedRoleFromStore != null },
+    );
+  const selected = selectedRoleFromStore ?? selectedRoleFrombackend;
+
   const [searchTerm, setSearchTerm] = useState('');
   const q = useDebouncedValue(searchTerm.trim());
   const { data, isLoading, isFetching } = useSearchRelationRolesQuery({ q });
   const roles = useMemo(() => {
-    return keyBy(data?.results ?? [], (e) => {
+    const roles = keyBy(data?.results ?? [], (e) => {
       return e.id;
     });
-  }, [data]);
-
-  const _stored = useAppSelector(selectVocabularyEntries);
+    if (selected != null) {
+      roles[selected.id] = selected;
+    }
+    return roles;
+  }, [data, selected]);
 
   function onInputChange(event: ChangeEvent<HTMLInputElement>) {
     setSearchTerm(event.currentTarget.value);
   }
 
   function getDisplayLabel(id: string) {
-    const role = roles[id] ?? _stored[id];
+    const role = roles[id] ?? storedRoles[id];
     if (role == null) return '';
     return getTranslatedLabel(role.label);
+  }
+
+  function onClear() {
+    field.input.onChange(undefined);
   }
 
   return (
     <FormField>
       <Label htmlFor={id}>{label}</Label>
-      <ComboBox disabled={isLoading} onValueChange={field.input.onChange} value={field.input.value}>
+      <ComboBox
+        disabled={isLoading || isInitialLoading}
+        onValueChange={field.input.onChange}
+        value={field.input.value}
+      >
         <ComboBoxTrigger>
           <ComboBoxInput
             displayValue={getDisplayLabel}
@@ -174,7 +225,12 @@ function RelationRoleFilter(): JSX.Element {
             onChange={onInputChange}
             placeholder={placeholder}
           />
-          {isFetching ? <LoadingIndicator className="mr-4 text-neutral-500" size="sm" /> : null}
+          {isFetching || isInitialLoading ? (
+            <LoadingIndicator className="mr-4 text-neutral-500" size="sm" />
+          ) : null}
+          {isNonEmptyString(field.input.value) && !isFetching && !isInitialLoading ? (
+            <ClearButton className="mr-4 text-neutral-500" onClick={onClear} />
+          ) : null}
           <ComboBoxButton />
         </ComboBoxTrigger>
         <ComboBoxContent>
@@ -243,5 +299,21 @@ function EntityKindFilter(): JSX.Element {
         </SelectContent>
       </Select>
     </FormField>
+  );
+}
+
+interface ClearButtonProps {
+  className?: string;
+  onClick: () => void;
+}
+
+function ClearButton(props: ClearButtonProps): JSX.Element {
+  const { className, onClick } = props;
+
+  return (
+    <button className={className} onClick={onClick}>
+      <span className="sr-only">Clear</span>
+      <XIcon className="h-4 w-4" />
+    </button>
   );
 }
