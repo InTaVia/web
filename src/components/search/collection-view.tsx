e2c4921df1fb249cd1d544b705661aaa5713a5ac
Samuel Beck <samuel.beck@vis.uni-stuttgart.de>
2023-07-04 11:56:08 +0200

    fix: selecting menu item no longer opens link to detail page (#216)
    - Stop propagation from onClick events from menu items to NextLink
    - Fix underlining of labels on hover

--- src/components/search/collection-view.tsx
+++ src/components/search/collection-view.tsx
@@ -10,6 +10,7 @@ import {
 } from '@intavia/ui';
 import { MenuIcon } from 'lucide-react';
 import NextLink from 'next/link';
+import type { MouseEvent } from 'react';
 import { useState } from 'react';
 
 import { useI18n } from '@/app/i18n/use-i18n';
@@ -23,6 +24,7 @@ import { NothingFoundMessage } from '@/components/nothing-found-message';
 import { useCollection } from '@/components/search/collection.context';
 import { CreateCollectionDialog } from '@/components/search/create-collection-dialog';
 import { EditEntityDialog } from '@/components/search/edit-entity-dialog';
+import { IntaviaIcon } from '@/features/common/icons/intavia-icon';
 import { getTranslatedLabel } from '@/lib/get-translated-label';
 
 export function CollectionView(): JSX.Element {
@@ -101,7 +103,6 @@ function CollectionEntity(props: CollectionEntityProps): JSX.Element | null {
   const _localEntities = useAppSelector(selectLocalEntities);
   const hasLocalEntity = id in _localEntities;
 
-  const [isHovered, setHovered] = useState(false);
   const [isDialogOpen, setDialogOpen] = useState(false);
 
   if (entity == null) return null;
@@ -117,24 +118,23 @@ function CollectionEntity(props: CollectionEntityProps): JSX.Element | null {
     dispatch(removeEntitiesFromCollection({ id: currentCollection, entities: [id] }));
   }
 
+  function onClick(e: MouseEvent) {
+    e.stopPropagation();
+  }
+
   return (
     <NextLink href={detailsUrl}>
-      <article
-        onMouseEnter={() => {
-          setHovered(true);
-        }}
-        onMouseLeave={() => {
-          setHovered(false);
-        }}
-        className="flex cursor-pointer items-center justify-between gap-8 px-8 py-2.5 hover:bg-slate-100"
-      >
-        <div className="grid gap-1 leading-tight">
-          <a className={isHovered ? 'underline' : ''}>
-            <span>{getTranslatedLabel(entity.label)}</span>
-            {hasLocalEntity ? <span> (edited locally)</span> : null}
-          </a>
-          <div className="text-xs text-neutral-500">
-            {t(['common', 'entity', 'kinds', entity.kind, 'one'])}
+      <article className="group flex cursor-pointer items-center justify-between gap-8 px-8 py-2.5 hover:bg-slate-100">
+        <div className="flex flex-row items-center justify-start gap-6">
+          <IntaviaIcon className="fill-none" icon={entity.kind} />
+          <div className="grid gap-1 leading-tight">
+            <a className="group-hover:underline">
+              <span>{getTranslatedLabel(entity.label)}</span>
+              {hasLocalEntity ? <span> (edited locally)</span> : null}
+            </a>
+            <div className="text-xs text-neutral-500">
+              {t(['common', 'entity', 'kinds', entity.kind, 'one'])}
+            </div>
           </div>
         </div>
 
@@ -147,10 +147,18 @@ function CollectionEntity(props: CollectionEntityProps): JSX.Element | null {
           </DropdownMenuTrigger>
 
           <DropdownMenuContent className="w-56">
-            <DropdownMenuItem disabled={currentCollection == null} onSelect={onEditItem}>
+            <DropdownMenuItem
+              disabled={currentCollection == null}
+              onClick={onClick}
+              onSelect={onEditItem}
+            >
               {t(['common', 'search', 'edit-item'])}
             </DropdownMenuItem>
-            <DropdownMenuItem disabled={currentCollection == null} onSelect={onRemoveItem}>
+            <DropdownMenuItem
+              disabled={currentCollection == null}
+              onClick={onClick}
+              onSelect={onRemoveItem}
+            >
               {t(['common', 'collections', 'remove-item'])}
             </DropdownMenuItem>
           </DropdownMenuContent>
