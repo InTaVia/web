2a47c194fef3e748ee10f4b75ceb137a7c0a21a0
Samuel Beck <samuel.beck@vis.uni-stuttgart.de>
2023-07-06 15:10:54 +0200

    feat: detail page map fitBounds, only show map if events have geometry

--- src/features/entities/use-entity-has-geometry.tsx
+++ src/features/entities/use-entity-has-geometry.tsx
@@ -1,35 +1,13 @@
-import type { Entity, Event } from '@intavia/api-client';
+import type { Entity } from '@intavia/api-client';
 
-import { useEntities } from '@/lib/use-entities';
-import { useEvents } from '@/lib/use-events';
+import { useRelatedEntities } from '@/lib/use-related-entities';
 
 export function useEntityHasGeometry(entity: Entity): boolean {
-  const relations = entity.relations;
-  if (relations.length <= 0) return false;
+  const relatedEntities = useRelatedEntities(entity);
 
-  const eventIds = relations.map((relation) => {
-    return relation.event;
-  });
-
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const { data, status } = useEvents(eventIds);
-  let hasGeometry = false;
-  if (status === 'success') {
-    data.forEach((event: Event) => {
-      const eventRelations = event.relations;
-      const entityIds = eventRelations.map((relation) => {
-        return relation.entity;
-      });
-
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      const { data, status } = useEntities(entityIds);
-      if (status === 'success') {
-        data.forEach((entity: Entity) => {
-          hasGeometry = entity.kind === 'place' && entity.geometry !== undefined;
-        });
-      }
-    });
+  for (const entity of relatedEntities) {
+    if (entity.kind === 'place' && entity.geometry !== undefined) return true;
   }
 
-  return hasGeometry;
+  return false;
 }
